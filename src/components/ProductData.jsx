f0970383763385582a0b875703fd76778a351042
himanshuhoney30 <himanshuhoney30@gmail.com>
2025-01-22 13:51:30 +0530

    Fifth commit

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -17,7 +17,7 @@ const ProductsData = [
     rating: 5.0,
     color: "white",
     aosDelay: "0",
-    price: 100.00, 
+    price: 100.0,
   },
   {
     id: 2,
@@ -26,7 +26,7 @@ const ProductsData = [
     rating: 4.5,
     color: "Red",
     aosDelay: "200",
-    price: 200.00,
+    price: 200.0,
   },
   {
     id: 3,
@@ -35,7 +35,7 @@ const ProductsData = [
     rating: 4.7,
     color: "brown",
     aosDelay: "400",
-    price: 300.00,
+    price: 300.0,
   },
   {
     id: 4,
@@ -44,7 +44,7 @@ const ProductsData = [
     rating: 4.4,
     color: "Yellow",
     aosDelay: "600",
-    price: 400.00,
+    price: 400.0,
   },
   {
     id: 5,
@@ -53,7 +53,7 @@ const ProductsData = [
     rating: 4.5,
     color: "Pink",
     aosDelay: "800",
-    price: 500.00,
+    price: 500.0,
   },
   {
     id: 6,
@@ -62,7 +62,7 @@ const ProductsData = [
     rating: 4.5,
     color: "Black",
     aosDelay: "800",
-    price: 600.00,
+    price: 600.0,
   },
   {
     id: 7,
@@ -71,7 +71,7 @@ const ProductsData = [
     rating: 4.5,
     color: "Blue",
     aosDelay: "800",
-    price: 700.00,
+    price: 700.0,
   },
   {
     id: 8,
@@ -80,7 +80,7 @@ const ProductsData = [
     rating: 4.5,
     color: "White",
     aosDelay: "800",
-    price: 800.00,
+    price: 800.0,
   },
   {
     id: 9,
@@ -89,7 +89,7 @@ const ProductsData = [
     rating: 4.5,
     color: "Sky Blue",
     aosDelay: "800",
-    price: 900.00,
+    price: 900.0,
   },
   {
     id: 10,
@@ -98,9 +98,92 @@ const ProductsData = [
     rating: 4.5,
     color: "Black",
     aosDelay: "800",
-    price: 1000.00,
-  }
+    price: 1000.0,
+  },
+  {
+    id: 11,
+    img: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    title:
+      "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    price: 109,
+    aosDelay: "800",
+    rating: 4.8,
+  },
+  {
+    id: 12,
+    title:
+      "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    price: 114,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    rating: 4.8,
+  },
+  {
+    id: 13,
+    title: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+    price: 599,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    rating: 2.9,
+  },
+  {
+    id: 14,
+    title:
+      "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+    price: 999.99,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    rating: 2.2,
+  },
+  {
+    id: 15,
+    title: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    price: 56.99,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    rating: 2.6,
+  },
+  {
+    id: 16,
+    title:
+      "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    price: 29.95,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    rating: 2.9,
+  },
+  {
+    id: 17,
+    title: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    price: 39.99,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    rating: 3.8,
+  },
+  {
+    id: 18,
+    title: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    price: 9.85,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    rating: 4.7,
+  },
+  {
+    id: 19,
+    title: "Opna Women's Short Sleeve Moisture",
+    price: 7.95,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    rating: 4.5,
+  },
+  {
+    id: 20,
+    title: "DANVOUY Womens T Shirt Casual Cotton Short",
+    price: 12.99,
+    aosDelay: "800",
+    img: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    rating: 3.6,
+  },
 ];
-
 
 export default ProductsData;
